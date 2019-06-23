050395dc05257fc5a694e8c9ba3583cc73ee6ac9
connormayfield <connormayfield@gmail.com>
2019-06-23 15:32:02 -0500

    images rendering on page

---
+++ recipe/src/App.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
+import "./App.css";
+
+const App = () => {
+  const APP_ID = "12751db6";
+  const APP_KEY = "9a4ba070c4fb804df029bf59adaac43e";
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+  return (
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      {recipes.map(recipe =>(
+        <Recipe
+        title={recipe.recipe.label}
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        />
+      ))};
+    </div>
+  );
+};
+export default App;
